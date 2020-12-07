50c4506be4aad5f99a11706f80b0a947ec8b4b8f
Antun Maldini <antun.maldini@fer.hr>
2020-12-07 21:40:00 +0100

    Add a mock origin server

---
+++ origin-server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import flask
+
+SERVER_NAMES = ["alpha", "bravo", "charlie", "delta"]
+BASE_URL = "http://localhost:8080/item/"
+app = flask.Flask("origin-server")
+
+@app.route("/item/<name>", methods=["GET"])
+def handle_request(name):
+    if name not in SERVER_NAMES:
+        return flask.Response("Nonexistent resource", 404)
+
+    if name in ("alpha", "bravo"):
+        return {"content": name, "expires_in": 120}
+    elif name == "charlie":
+        return {"content": "Hi, charlie!", "expires_in": 30}
+    else:
+        return {"content": "Delta David is the content", "expires_in": 200}
+
+if __name__ == "__main__":
+    # Hand off to gunicorn
+    import os, shutil
+    executable = shutil.which("gunicorn")
+    os.execve(executable, ["ignored", "--bind=0.0.0.0:8080", "origin-server:app"], os.environ)
